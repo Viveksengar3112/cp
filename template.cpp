440b970be3f6f9c326822650a6204f75265de560
Jatin Bhatnagar <31108241+jatinbhatnagar05@users.noreply.github.com>
2019-07-27 16:27:32 +0530

    template for competitive programming

---
+++ template.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define FIO ios_base::sync_with_stdio(0); cin.tie(NULL)
+#define pb push_back
+#define pii pair<int,int>
+#define ll long long int 
+#define ui unsigned int
+#define pll pair<ll,ll> 
+#define mp make_pair
+#define f first
+#define s second
+#define all(x) (x).begin(),(x).end() 
+#define SORT(v) sort(all(v))
+#define SORTD(v) sort(all(v),greater<int>())
+#define SET(a,b) for(ui i=0;i<size(a);++i){a[i]=b;} 
+#define test() int t; cin>>t ; while(t--)
+#define FOR(i,a,b) for(int i=a;i<b;++i)  
+#define cerr if(0)cerr
+#define watch(x) cerr<<#x<<" is "<<x<<endl;
+#define print(x) cout<<x<<endl
+#define readv(v) FOR(i,0,n){cin>>v[i];}
+#define printv(v) for(auto i : v){cout<<i<<" ";}cout<<endl;
+const ll MOD=1e9+7;
+ll powmod(ll a,ll b) {ll res=1;a%=MOD; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%MOD;a=a*a%MOD;}return res;}
+ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
+
+
+//_________________________________________________________________________________
+
+using namespace std;
+
+
+int main()
+{
+	#ifdef LOCAL
+	auto start_time = clock();
+	cout << setprecision(3) << fixed;
+	#endif
+	
+	FIO;
+	
+//~ ________________________________________________________________________________________
+	
+	
+	/*
+		start here.
+	*/
+		
+	
+	
+	
+	
+	
+	
+
+//~ ________________________________________________________________________________________
+	#ifdef LOCAL
+	auto end_time = clock();
+	cout<<endl;
+	cout << "Execution time: " << (end_time - start_time) * (int)1e3 / CLOCKS_PER_SEC << " ms\n";
+	#endif
+	
+	return 0;
+
+}
+	
+	
+
+
+
+
+
